10634a0e1d9b7549f2f9d292e0f09be794b19a01
sj-cheon <dnwjd0504@naver.com>
2024-01-07 18:49:39 +0900

    ed: publish

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hello, World!");
+const fs = require('fs');
+const { app, BrowserWindow } = require('electron')
+
+fs.symlink('package.json', 'yes', console.log);
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
